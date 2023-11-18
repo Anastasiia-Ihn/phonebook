bae29c9dc15abad78799d4c8fa6d1c9da6ed0c91
Anastasiia <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-11-18 17:02:01 +0100

    added isRefreshing

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
-  padding: 15px;
+  padding: 0;
   margin: 0 auto;
 `;
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,18 @@
+import { useSelector } from 'react-redux';
 import { ListNavStyled, StyledLink } from './Navigation.styled';
+import { selectIsLoggedIn } from 'redux/auth/selectors';
 
-export const Navigation = () => (
-  <nav>
+export const Navigation = () => {
+  const  isLoggedIn  = useSelector(selectIsLoggedIn);
+
+  return <nav>
     <ListNavStyled>
       <li>
         <StyledLink to="/">Home</StyledLink>
       </li>
-      <li>
+      {isLoggedIn && (<li>
         <StyledLink to="/contacts">Contacts</StyledLink>
-      </li>
+      </li>)}
     </ListNavStyled>
   </nav>
-);
+};
